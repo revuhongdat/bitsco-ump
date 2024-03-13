6750d3879ef74c79938b6c84ad97f8ba971f7a96
revuhongdat <revuhongdat@gmail.com>
2024-03-13 13:13:40 +0700

    update filter
    - lấy data từ CMS

--- inc/home-widget-filter.php
+++ inc/home-widget-filter.php
@@ -16,121 +16,77 @@ class HomeWidgetFilter extends WP_Widget
     function widget($args, $instance)
     {
         extract($args);
-
+    
         $title = apply_filters('title', isset($instance['title']) ? esc_attr($instance['title']) : '');
-        $category = apply_filters('category', isset($instance['category']) ? esc_attr($instance['category']) : '');
+        $selected_categories = isset($instance['category']) ? $instance['category'] : array();
         $count = apply_filters('count', isset($instance['count']) && is_numeric($instance['count']) ? esc_attr($instance['count']) : '');
-
-        $category_link = get_category_link($category);
-
+    
+        // Hiển thị phần trước widget
         echo $before_widget;
 
-        $wp_query = new WP_Query(array(
-            'cat' => $category,
-            'posts_per_page' => $count,
-        ));
-        ?>
-            
-    <div class="filter-block mt-5">
-        <div>
-            <h4 class="text-center">Programs and Initiatives</h4>
-            <p class="text-center">Welcome to the City of Dream</p>
-        </div>
-
-        <div id="filters" class="button-group d-flex justify-content-center align-items-center">  
-            <button class="button is-checked" data-filter="*">All Categories</button>
-            <button class="button" data-filter=".eco">Eco</button>
-            <button class="button" data-filter=".programs">Programs</button>
-            <button class="button" data-filter=".social-life, .programs">Social life</button>
-            <button class="button" data-filter=":not(.programs)">Sport</button>
-            <button class="button" data-filter=".eco:not(.programs), .technology">Technology</button>
-            <!-- <button class="button" data-filter="numberGreaterThan50">number > 50</button>
-            <button class="button" data-filter="ium">name ends with &ndash;ium</button> -->
-        </div>
-
-        <div class="grid row">
-            <div class="col-4 element-item programs eco " data-category="eco">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/5253789_l-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Consumer Protection</div>
-                    <div class="my-category">in Programs, Technology</div>
-                </div>
-            </div>
-
-            <div class="col-4 element-item sport eco " data-category="sport">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/baseball-player-pitcher-ball-163487-large-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Sport Life</div>
-                    <div class="my-category">in Programs, Sport</div>
-                </div> 
-            </div>
-
-            <div class="col-4 element-item social-life technology eco" data-category="social-life">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/Depositphotos_46326837_original-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Education</div>
-                    <div class="my-category">in Programs, Social life</div>
-                </div>
-            </div>
-
-            <div class="col-4 element-item technology sport" data-category="technology">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/Depositphotos_69019339_original-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Grants</div>
-                    <div class="my-category">in Programs, Social life</div>
-                </div>
-            </div>
-
-            <div class="col-4 element-item programs technology" data-category="programs">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/Depositphotos_23729097_original-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Environment</div>
-                    <div class="my-category">in Programs, Social life</div>
-                </div>
-            </div>
-
-            <div class="col-4 element-item technology " data-category="technology">
-                <div class="item-container">
-                    <img class="item-img" src="https://dream-city.cmsmasters.net/wp-content/uploads/2015/01/Depositphotos_88884488_original-s.jpg" alt="">
-                </div>
-                <div class="item-footer">
-                    <div class="my-symbol">Technology</div>
-                    <div class="my-category">in Programs</div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-            <?php
+        echo '<div class="filter-block mt-5">';
+        echo '<div>';
+        echo '<h4 class="text-center">Programs and Initiatives</h4>';
+        echo '<p class="text-center">Welcome to the City of Dream</p>';
+        echo '</div>';
+        echo '<div id="filters" class="button-group d-flex justify-content-center align-items-center">';
+        echo '<button class="button is-checked" data-filter="*">All Categories</button>';
+        foreach ($selected_categories as $category_id) {
+            $category = get_category($category_id);
+            if ($category) {
+                $category_slug = $category->slug;
+                $category_name = $category->name;
+                echo '<button class="button" data-filter=".' .$category_slug. '">' .$category_name. '</button>';
+            }
+        }
+        echo '</div>';
+        echo '<div class="grid row">';
+        foreach ($selected_categories as $category_id) {
+            $category = get_category($category_id);
+            if ($category) {
+                $category_slug = $category->slug;
+                $category_name = $category->name;
+                // Tạo một truy vấn WordPress để lấy bài viết từ chuyên mục hiện tại
+                $wp_query = new WP_Query(array(
+                    'cat' => $category_id,
+                    'posts_per_page' => $count,
+                ));
+                // Lặp qua các bài viết trong truy vấn và hiển thị tiêu đề và ảnh của mỗi bài viết
+                while ($wp_query->have_posts()) : $wp_query->the_post();
+                    echo '<div class="col-4 element-item programs ' .$category_slug. '" data-category="' .$category_slug. '">';
+                    echo '<div class="item-container">';
+                    if (has_post_thumbnail()) {
+                        // Nếu bài viết có ảnh đại diện, hiển thị nó
+                        the_post_thumbnail('thumbnail', ['class' => 'item-img', 'alt' => get_the_title()]);
+                    }
+                    echo '</div>';
+                    echo '<div class="item-footer text-center">';
+                    echo '<a class="my-symbol text-decoration-none" href="' . get_permalink() . '" class="card-title">' . get_the_title() . '</a>';
+                    echo '<div class="my-category">Thuộc ' .$category_name. '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                endwhile;
+                // Đặt lại truy vấn và dữ liệu bài viết
                 wp_reset_query();
-                wp_reset_postdata();
-            ?>
-        <?php
+                wp_reset_postdata();      
+            }
+        }
+        echo '</div>';
+        echo '</div>';
         echo $after_widget;
         
     }
     function update($new_instance, $old_instance)
     {
-        // Save widget options
+        // Lưu các tùy chọn mới từ form
         $instance = $old_instance;
-
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['category'] = strip_tags($new_instance['category']);
-        $instance['count'] = strip_tags($new_instance['count']);
-
-
+        // $instance['title'] = strip_tags($new_instance['title']);
+        $instance['category'] = isset($new_instance['category']) ? $new_instance['category'] : array();
+        // $instance['count'] = strip_tags($new_instance['count']);
+    
         return $instance;
     }
+    
 
     function form($instance)
     {
@@ -138,40 +94,36 @@ class HomeWidgetFilter extends WP_Widget
         $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
         $category = isset($instance['category']) ? esc_attr($instance['category']) : '';
         $count = isset($instance['count']) && is_numeric($instance['count']) ? (int)$instance['count'] : 4;
-        ?>
+        // Lấy tất cả các chuyên mục
+        $all_categories = get_categories(array('hide_empty' => 0, 'taxonomy' => 'category', 'hierarchical' => 1));
+    ?>
+    <!-- Form hiển thị trên UI -->
+    <!-- Select option category -->
+    <p>
+    <label for="<?php echo $this->get_field_id('category'); ?>">
+    <?php _e('Chuyên mục:', 'iboss'); ?>
+    <!-- Bắt đầu nhãn cho các checkbox, liên kết với id của trường -->
 
-        <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>">
-                <?php _e('Tiêu đề:', 'iboss'); ?>
-                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-                       name="<?php echo $this->get_field_name('title'); ?>" type="text"
-                       value="<?php echo $title; ?>"/>
-            </label>
-        </p>
+    <?php
+    $cats = get_categories(array('hide_empty' => 0, 'taxonomy' => 'category', 'hierarchical' => 1));
+    // Lấy danh sách tất cả các chuyên mục
 
-        <p>
-            <label for="<?php echo $this->get_field_id('category'); ?>">
-                <?php _e('Chuyên mục:', 'iboss'); ?>
-                <select class="widefat" id="<?php echo $this->get_field_id('category'); ?>"
-                        name="<?php echo $this->get_field_name('category'); ?>">
-                    <?php echo '<option value="0" ' . ('0' == $category ? 'selected="selected"' : '') . '>' . __('Tất cả') . '</option>';
-                    $cats = get_categories(array('hide_empty' => 0, 'taxonomy' => 'category', 'hierarchical' => 1));
-                    foreach ($cats as $cat) {
-                        echo '<option value="' . $cat->term_id . '" ' . ($cat->term_id == $category ? 'selected="selected"' : '') . '>' . $cat->name . '</option>';
-                    }
-                    ?>
-                </select>
-            </label>
+    foreach ($cats as $cat) {
+        // Bắt đầu vòng lặp qua từng chuyên mục
+        $checked = in_array($cat->term_id, (array)$category) ? 'checked="checked"' : '';
+        // Kiểm tra xem chuyên mục có được chọn hay không
+
+        echo '<p>';
+        echo '<input type="checkbox" id="' . esc_attr($this->get_field_id('category') . '_' . $cat->term_id) . '" name="' . esc_attr($this->get_field_name('category')) . '[]" value="' . esc_attr($cat->term_id) . '" ' . $checked . '>';
+        echo '<label for="' . esc_attr($this->get_field_id('category') . '_' . $cat->term_id) . '">' . esc_html($cat->name) . '</label>';
+        echo '</p>';
+        // Tạo checkbox và nhãn tương ứng cho từng chuyên mục
+    }
+    ?>
+
+</label>
         </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('count'); ?>">
-                <?php _e('Số bài viết (tối đa 4):', 'iboss'); ?>
-                <input id="<?php echo $this->get_field_id('count'); ?>"
-                       name="<?php echo $this->get_field_name('count'); ?>" type="number"
-                       min="1" max="4" value="<?php echo $count; ?>"/>
-            </label>
-        </p>
-        <?php
+    <?php
     }
 }
 
